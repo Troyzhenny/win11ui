cc7738a88cb839a82a54a2f5dc5c51920c5db4a6
Tevin Campbell <tevincampbell73@yahoo.com>
2024-03-29 18:16:15 -0400

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Taskbar from "./components/Taskbar";
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <h1>Hello World!</h1>
+        <Taskbar />
+      </div>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Taskbar.jsx
@@ -0,0 +1,13 @@
+import "./taskbar.css";
+
+const Taskbar = () => {
+    return(
+        <>
+            <div className="taskbar--container">
+                <h1>Taskar</h1>
+            </div>
+        </>
+    )
+}
+
+export default Taskbar;
