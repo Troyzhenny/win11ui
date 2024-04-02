4c7850f25d57809a0cfde1108a71e3ad59cecbaa
Tevin Campbell <tevincampbell73@yahoo.com>
2024-04-01 22:48:55 -0400

    taskbar  icon hover state + layout

--- src/components/Taskbar/Taskbar.jsx
+++ src/components/Taskbar/Taskbar.jsx
@@ -1,11 +1,26 @@
 import "../Taskbar/taskbar.css";
+import winKey from "/src/assets/icons/w11-100.png";
+import fileExplorer from "/src/assets/icons/fe-100.png";
+import vsCode from "/src/assets/icons/vsc-100.png";
+import mEdge from "/src/assets/icons/edge-100.png";
+
 
 const Taskbar = () => {
     return(
         <>
             <div className="taskbar--container">
-                <div>apps</div>
-                <div>util</div>
+                <div className="leftBox">
+                    <p>b1</p>
+                </div>
+                <div className="centerBox">
+                    <img id="taskIcons" src={winKey} alt="desc" />
+                    <img id="taskIcons" src={fileExplorer} alt="desc" />
+                    <img id="taskIcons" src={vsCode} alt="desc" />
+                    <img id="taskIcons" src={mEdge} alt="desc" />
+                </div>
+                <div className="rightBox">
+                    <p>b3</p>
+                </div>
             </div>
         </>
     )
